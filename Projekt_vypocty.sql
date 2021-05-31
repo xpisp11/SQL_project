4a858a3330a75c74d5a238dc93ca35e8266521f5
Petra Rohlickova <peta.rohlickova@gmail.com>
2021-05-31 20:31:43 +0200

    mirne upravy

--- Projekt_vypocty.sql
+++ Projekt_vypocty.sql
@@ -1,19 +1,35 @@
--- binární promìnná pro víkend / pracovní den
+-- Výpoètu pro tvorbu dalších sloupcù
+
 SELECT
 	*,
+	-- binární promìnná pro víkend / pracovní den
 	CASE 
-		WHEN WEEKDAY(`date`) IN (5, 6) THEN 1 
+		WHEN WEEKDAY(datum) IN (5, 6) THEN 1 
 		ELSE 0 
 		END AS "Víkend",
+	-- roèní období
 	CASE 
-		WHEN `date` < '2020-03-20' OR (`date` BETWEEN '2020-12-21' AND '2021-03-19') THEN "zima"
-		WHEN `date` < '2020-06-20' OR (`date` BETWEEN '2021-03-20' AND '2021-06-20') THEN "jaro"
-		WHEN `date` < '2020-09-22' THEN "léto"
-		WHEN `date` < '2020-12-21' THEN "podzim"
+		WHEN datum < '2020-03-20' OR (datum BETWEEN '2020-12-21' AND '2021-03-19') THEN "zima"
+		WHEN datum < '2020-06-20' OR (datum BETWEEN '2021-03-20' AND '2021-06-20') THEN "jaro"
+		WHEN datum < '2020-09-22' THEN "léto"
+		WHEN datum < '2020-12-21' THEN "podzim"
 		END AS "Roèní období"
-FROM v_joined_covid_lookup_economies_countries
-WHERE country = 'Afghanistan';
+FROM v_joined_covid_lookup_tests_economies_countries
+WHERE ISO = 'USA'
+;
+
+-- Výpoèty v tabulce weather
+-- a) prùmìrná denní (nikoli noèní!) teplota
+
+SELECT 
+	`time`,
+	`date`,
+
+-- b) poèet hodin v daném dni, kdy byly srážky nenulové
+-- c) maximální síla vìtru v nárazech bìhem dne
 
 
-SELECT * FROM weather GROUP BY city;
-SELECT * FROM countries;  -- odtud vzít capital city a pøes to dohledat v tabulce weather poèasí
+SELECT * FROM weather
+
+SELECT DISTINCT city FROM weather ORDER BY city;
+SELECT capital_city FROM countries WHERE capital_city IS NOT NULL ORDER BY capital_city;  -- odtud vzít capital city a pøes to dohledat v tabulce weather poèasí
